fa73241f63b4e927cb3956d3d728a2f0ccc81f7d
san4d <rallensanford@protonmail.com>
2020-12-29 11:12:52 -0800

    Record TransferStart on Ethereum
    Start a transfer without destination tag
    
    Allow destination tag to be specified
    
    Reference contract enums directly in tests
    
    Add balance testing
    
    Add inline comments for tests

--- ethereum/contracts/Bridge.sol
+++ ethereum/contracts/Bridge.sol
@@ -2,5 +2,58 @@
 pragma solidity ^0.8.0;
 
 contract Bridge {
-    string public constant hello = "hello";
+    enum TransferStatus {Started, Succeeded, Failed}
+
+    struct Transfer {
+        uint id;
+        address sourceAddress;
+        uint value;
+        string xrplDestinationAddress;
+        string xrplDestinationTag;
+        TransferStatus status;
+    }
+
+    event TransferStarted(uint _id);
+
+    address payable public owner;
+    uint numTransfers;
+    mapping(uint => Transfer) transfersById;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function getTransfer(uint _id) public view returns (Transfer memory) {
+		return transfersById[_id];
+	}
+
+    ///
+    /// Start a transfer
+    ///
+
+    function startTransfer(string memory _xrplAddress, string memory _xrplTag) payable public {
+        startTransfer(_xrplAddress, _xrplTag, msg.sender, msg.value);
+    }
+
+    function startTransfer(string memory _xrplAddress) payable public {
+        startTransfer(_xrplAddress, "", msg.sender, msg.value);
+    }
+
+    function startTransfer(
+        string memory _xrplAddress, 
+        string memory _xrplTag,
+        address _sourceAddress,
+        uint _value
+    ) internal {
+        Transfer storage transfer = transfersById[++numTransfers];
+
+        transfer.id = numTransfers;
+        transfer.sourceAddress = _sourceAddress;
+        transfer.value = _value;
+        transfer.xrplDestinationAddress = _xrplAddress;
+        transfer.xrplDestinationTag = _xrplTag;
+        transfer.status = TransferStatus.Started;
+
+        emit TransferStarted(transfer.id);
+    }
 }
--- ethereum/test/bridge.js
+++ ethereum/test/bridge.js
@@ -1,9 +1,115 @@
 const Bridge = artifacts.require("Bridge");
 
-contract("Bridge", _accounts => {
-    it("should say hello", () =>
-        Bridge.deployed()
-            .then(instance => instance.hello())
-            .then(msg => assert.equal(msg, "hello")));
-    }
-);
+let bridge;
+
+const toTransferObject = tuple => {
+    assert.equal(tuple.length, 6, "Transfer contains six items");
+    return {
+        id: tuple[0],
+        sourceAddress: tuple[1],
+        value: tuple[2],
+        xrplDestinationAddress: tuple[3],
+        xrplDestinationTag: tuple[4],
+        status: tuple[5]
+    };
+};
+
+const getBalance = async ({address}) => await web3.eth.getBalance(address).then(parseInt);
+
+contract("Bridge", accounts => {
+
+    before(async () => {
+        bridge = await Bridge.deployed();
+        let presuiteBalance = await getBalance(bridge);
+        assert.equal(presuiteBalance, 0);
+    })
+
+    it("should be owned by the deploying address", async () => {
+        const owner = await bridge.owner.call()
+        assert.equal(owner, accounts[0]);
+    });
+    
+    it("should return empty transfer for nonexistent id", async () => {
+        const nullAddress = "0x0000000000000000000000000000000000000000";
+        
+        const transfer = await bridge.getTransfer.call(0)
+                                .then(toTransferObject);
+
+        assert.equal(transfer.id, 0);
+        assert.equal(transfer.sourceAddress, nullAddress);
+        assert.equal(transfer.value, 0);
+        assert.equal(transfer.xrplDestinationAddress, "");
+        assert.equal(transfer.xrplDestinationTag, "");
+        assert.equal(transfer.status, Bridge.TransferStatus.Started);
+
+        assert.equal(await getBalance(bridge), 0);
+    });
+
+    it("should start a transfer with an empty tag when not specified", async () => {
+        // Arrange
+        const xrplAddr = "rAsDwEr7kfTD9w2To4CQk6UCfuHM9c6GDY";
+        const data = {
+            from: accounts[1],
+            value: 10
+        }
+        const initialBalance = await getBalance(bridge);
+
+        // Act: start transfer
+        const {logs} = await bridge.methods["startTransfer(string)"](xrplAddr, data);
+
+        // Assert: TransferStarted event
+        assert.equal(logs.length, 1);
+        const startedEvent = logs[0];
+        assert.equal(startedEvent.event, "TransferStarted");
+
+        // Assert: bridge balance
+        assert.equal(await getBalance(bridge), data.value + initialBalance);
+
+        // Act: get transfer
+        const transfer = await bridge.getTransfer(startedEvent.args._id)
+                                    .then(toTransferObject);
+
+        // Assert: transfer data
+        assert.equal(transfer.id, startedEvent.args._id);
+        assert.equal(transfer.sourceAddress, data.from);
+        assert.equal(transfer.value, data.value);
+        assert.equal(transfer.xrplDestinationAddress, xrplAddr);
+        assert.equal(transfer.xrplDestinationTag, "");
+        assert.equal(transfer.status, Bridge.TransferStatus.Started);
+    });
+
+    it("should start a transfer given an address and tag", async () => {
+        // Arrange
+        const xrplAddr = "rAsDwEr7kfTD9w2To4CQk6UCfuHM9c6GDY";
+        const xrplTag = "1234"
+        const data = {
+            from: accounts[1],
+            value: 10
+        }
+        const initialBalance = await getBalance(bridge);
+        
+        // Act: start transfer
+        const {logs} = await bridge.startTransfer(xrplAddr, xrplTag, data);
+
+        // Assert: TransferStarted event
+        assert.equal(logs.length, 1);
+        const startedEvent = logs[0];
+        assert.equal(startedEvent.event, "TransferStarted");
+
+        // Assert: bridge balance
+        assert.equal(await getBalance(bridge), data.value + initialBalance);
+
+
+        // Act: get transfer
+        const transfer = await bridge.getTransfer(startedEvent.args._id)
+                                    .then(toTransferObject);
+
+        // Assert: transfer data
+        assert.equal(transfer.id, startedEvent.args._id);
+        assert.equal(transfer.sourceAddress, data.from);
+        assert.equal(transfer.value, data.value);
+        assert.equal(transfer.xrplDestinationAddress, xrplAddr);
+        assert.equal(transfer.xrplDestinationTag, xrplTag);
+        assert.equal(transfer.status, Bridge.TransferStatus.Started);
+    });
+})
